d5974112c3aae8c7f2a212b671394f2b8c786608
France Dev <france.dev@outlook.com>
2026-06-10 22:24:46 -0300

    Comment out Newsletter component in HomePage for future consideration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function HomePage() {
       <FeatureSection />
       <Testimonials />
       <CTABanner />
-      <Newsletter />
+      {/* <Newsletter /> */}
       <Footer />
     </main>
   );
